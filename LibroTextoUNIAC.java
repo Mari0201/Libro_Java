0f20dd9f4adf1bf37d2cedbe826a1e7df276a433
Mari0201 <latorremariana84@gmail.com>
2026-03-09 21:01:06 -0500

    LibroTexto

---
+++ LibroTextoUNIAC.java
@@ -0,0 +1,13 @@
+public class LibroTextoUNIAC extends LibroTexto {
+
+    private String facultad;
+
+    public LibroTextoUNIAC(String titulo, String autor, int numeroEjemplares, int numeroPrestados, String curso, String facultad) {
+        super(titulo, autor, numeroEjemplares, numeroPrestados, curso);
+        this.facultad = facultad;
+    }
+
+    public String toString() {
+        return super.toString() + "\Facultad: " + facultad;
+    }
+}
