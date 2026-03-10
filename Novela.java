3dc1de8c972256772312f8695c2c6faab96bd07e
Mari0201 <latorremariana84@gmail.com>
2026-03-09 21:01:26 -0500

    Novela_Java

---
+++ Novela.java
@@ -0,0 +1,14 @@
+public class Novela extends Libro {
+
+    private String tipo;
+
+    public Novela(String titulo, String autor, int numeroEjemplares, int numeroPrestados, String tipo) {
+        super(titulo, autor, numeroEjemplares, numeroPrestados);
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nTipo de novela: " + tipo;
+    }
+}
